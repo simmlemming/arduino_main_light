e3da44471c18daa733598f23383d1003aec77796
Maxim Tkachenko <mtkachenko@ebay.com>
2019-01-20 14:04:05 +0100

    Led brightness is changing. Also, current room brightness is showed on display.

--- main_light.ino
+++ main_light.ino
@@ -7,6 +7,8 @@
 #define BTN_UP 12
 #define BTN_ON 11
 #define BTN_DOWN 10
+#define LED 3
+#define PHOTO_RES A0
 
 #define BR_MIN 0
 #define BR_MAX 9
@@ -14,28 +16,50 @@
 #define OLED_RESET 4
 Adafruit_SSD1306 display(OLED_RESET);
 
-int br = 0;
+int br = 5;
+int photo_res = 0;
 
 Button btn_up = Button(BTN_UP, on_up_click);
 Button btn_down = Button(BTN_DOWN, on_down_click);
 
 void setup() {
   Serial.begin(9600);
+  pinMode(LED, OUTPUT);
+  pinMode(PHOTO_RES, INPUT_PULLUP);
+
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display.clearDisplay();
 
-  display.setTextSize(6);
   display.setTextColor(WHITE);
 }
 
 void loop() {
+  photo_res = analogRead(PHOTO_RES) / 10;
+
   btn_up.loop();
   btn_down.loop();
-  
+
   display.clearDisplay();
+
   display.setCursor(48, 16);
-  display.println(br);
+  display.setTextSize(6);
+  display.print(br);
+
+  display.setCursor(0, 0);
+  display.setTextSize(1);
+  display.print(photo_res);
   display.display();
+
+  int pwm = map(br, 0, 9, 0, 255);
+  Serial.print(br);
+  Serial.print(" -> ");
+  Serial.println(pwm);
+
+  update_led(pwm);
+}
+
+void update_led(int value) {
+  analogWrite(LED, value);
 }
 
 void on_up_click() {
